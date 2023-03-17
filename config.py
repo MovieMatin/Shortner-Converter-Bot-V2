5288ba4ce8d6d1649892a1bafc9806fcc89df741
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2023-03-17 22:06:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME", "Converter")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Matin:QAZ1qaz@QAZ1qaz@@cluster0.ygefm.mongodb.net/?retryWrites=true&w=majority") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "1963114305")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(1963114305)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001684353224")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "MPlayLink") # For Force Subscription
